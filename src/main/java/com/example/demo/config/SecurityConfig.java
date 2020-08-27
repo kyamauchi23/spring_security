2865241bb83adbee55d90cb6bcabde04b0ca057f
ikedaaya <ikeda.aya23@gmail.com>
2020-08-27 16:24:05 +0900

    Spring Security #2 ユーザー認証

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +23,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //  (3) 主にURLごとに異なるセキュリティ設定を行う
         http.authorizeRequests().antMatchers("/lp").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
+        http.formLogin()
+                .defaultSuccessUrl("/success");
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password(encoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder.encode("adminpassword")).roles("ADMIN");
+    }
 }
---
+++ src/main/java/com/example/demo/controller/SpringSecurityController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SpringSecurityController {
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+
+    @GetMapping("/lp")
+    public String lp(){
+        return "lp";
+    }
+
+    @GetMapping("/admin")
+    public String admin(@AuthenticationPrincipal User user){
+        System.out.println(user);
+        return "admin/admin";
+    }
+}
