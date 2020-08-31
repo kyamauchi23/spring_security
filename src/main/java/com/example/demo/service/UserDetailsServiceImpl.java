8c3b566eb8909accc84b1cd9a8ad33e8dace42e0
ikedaaya <ikeda.aya23@gmail.com>
2020-08-31 13:17:46 +0900

    UserDetailsServiceの実装

--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,28 +1,24 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Account;
-import com.example.demo.repository.AccountRepository;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private AccountRepository accountRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public UserDetailsServiceImpl(AccountRepository accountRepository){
-        this.accountRepository = accountRepository;
+    public UserDetailsServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -32,18 +28,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("username is empty");
         }
 
-        Account account = accountRepository.findByUsername(username);
+        Optional<User> user = userRepository.findById(username);
 
-        if (account == null) {
-            throw new UsernameNotFoundException("Not Found username " + username);
-        }
-
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(account.getRole()));
-
-        User user = new User(account.getUsername(), account.getPassword(), authorities);
-
-        return user;
-
+        return new UserDetailsImpl(user.orElseThrow(() -> new UsernameNotFoundException("Not Found username " + username)));
     }
 }
